7d42ce4abe4ab054c982e95564f10241db9f79b2
soxhnf <souhailhanaficlub136@gmail.com>
2026-01-02 15:27:09 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'audio-transcription',
+  webDir: 'www'
+};
+
+export default config;
